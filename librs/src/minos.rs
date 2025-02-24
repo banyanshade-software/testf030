4b35f0abb264815e9d8a2301e13b00f62ca00e3e
banyanshade-software <braun.daniel@free.fr>
2025-02-24 07:14:25 +0100

    .

--- librs/src/minos.rs
+++ librs/src/minos.rs
@@ -1,13 +1,14 @@
 
 //#![no_std]
-#![cfg_attr(not(test), no_std)]
+//#![cfg_attr(not(test), no_std)]
+
 #[cfg(test)]
 extern crate std;
 
 
 //#[cfg(not(test))]
 //extern crate critical_section;
-use core::ffi::c_int;
+//use core::ffi::c_int;
 use core::sync::atomic::{compiler_fence, Ordering};
 #[cfg(not(test))]
 use cortex_m::{peripheral};
@@ -21,18 +22,94 @@ pub extern "C" fn rs_main() -> !{
     }
 }
 
-static mut tickcnt :u32 = 0;
+static mut TICKCNT :u32 = 0;
 
 #[no_mangle]
 //#[interrupt]
+//#[exception]
 pub unsafe extern "C"  fn SysTick_Handler() {
-	tickcnt += 1;	
-    if 0 == (tickcnt % 300) {
+	TICKCNT += 1;	
+    if 0 == (TICKCNT % 300) {
         cortex_m::peripheral::SCB::set_pendsv();
     }
 }
+
+pub unsafe extern "C"  fn minos_yield() {
+	
+        cortex_m::peripheral::SCB::set_pendsv();
+        /* it is unclear if a barrier is actually needed here
+         * (spec for single core cortex-m)
+         * most RTOS/Scheduler put it while saying it's not mandatory 
+         * so we'll copy FreeRTOS here : "Barriers are normally not required but do ensure the code is 
+         * completely within the specified behaviour for the architecture"
+         */
+        cortex_m::asm::dsb();
+	    cortex_m::asm::isb();
+	    // or should we use compiler fence ??
+}
+
+// https://github.com/mychenkaikai/neon-rtos/blob/master/src/kernel/task/tcb.rs
+//#[repr(u16)]
+enum TaskState {
+    Ready,
+    Running,
+    Blocked //(BlockReason),
+}
+
+// TCB is in two part, one r/o which can
+// go in flash, and one rw in RAM.
+
+pub struct TCB_rw {
+    pub(crate) stack_top:  usize,
+    pub(crate) stack_addr: usize,
+}
+
+pub struct TCB_ro {
+    pub(crate) state: TaskState,
+    pub(crate) stack_size: usize,
+    pub(crate) base_priority: u8,
+}
+
 
 #[no_mangle]
 pub   extern "C" fn PendSV_Handler() {
 	
 }
+
+//             concat_idents!(minos, $name)($($arg_name),*)
+
+use paste::paste;
+
+#[macro_export]
+macro_rules! syscall {
+    ($name:ident($($arg_name:ident : $arg_type:ty),*)) => {
+        pub fn $name($($arg_name: $arg_type),*) {
+            paste::paste!{
+                [<minos_ $name>]($($arg_name),*);
+            }
+        }
+    };
+ 
+}
+
+// Use the macro to define the function
+
+syscall!(wait_notif(l: u32));
+syscall!(send_notif(tasknum:u8, l: u32));
+syscall!(wait());
+syscall!(wakeup(tasknum:u8));
+
+pub fn minos_wait_notif(_l :u32) {
+}
+
+pub fn minos_send_notif(_t:u8, _l :u32) {
+}
+
+
+pub fn minos_wait() {
+}
+
+pub fn minos_wakeup(tasknum:u8) {
+}
+
+
