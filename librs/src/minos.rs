2d9f919f9ad4c9c763b6f996dc7ae69d396a6831
banyanshade-software <braun.daniel@free.fr>
2025-02-26 08:11:33 +0100

    ..

--- librs/src/minos.rs
+++ librs/src/minos.rs
@@ -60,31 +60,81 @@ enum TaskState {
 // go in flash, and one rw in RAM.
 
 pub struct TCB_rw {
-    pub(crate) stack_top:  usize,
-    pub(crate) stack_addr: usize,
+	pub(crate) state: TaskState,
+    pub(crate) stack_top:  ptrsize,
+    pub(crate) stack_addr: ptrsize,
 }
 
 pub struct TCB_ro {
-    pub(crate) state: TaskState,
-    pub(crate) stack_size: usize,
+    pub(crate) stack_size: ptrsize,
     pub(crate) base_priority: u8,
 }
+
 
 pub struct Minos_Scheduler {
 	tasks_defs: &'static[TCB_ro],
 	tasks_vars: &'static mut[TCB_rw]
 }
 
+// https://github.com/garasubo/erkos/blob/f972248092816d7b2c084f09c6178908af9cd14c/kernel/src/macros.rs#L4
+
+
+macro_rules! stack_allocate {
+    ($nwords:expr) => {{
+        #[link_section = ".bss"]  // .uninit
+        static mut STACK: [ptrsize; $nwords] = [0; $nwords];
+
+        unsafe { &STACK[0] as *const ptrsize as ptrsize }
+    }};
+}
+
+/*impl TCB_rw {
+	pub const fn nc(stacksize: u32) -> Self {
+		//let st = stack_allocate!(stacksize);
+		#[link_section = ".bss"]  // .uninit
+        static mut STACK: [u32; stacksize] = [0; stacksize];
+        let st = unsafe { &STACK[0] as *const u32 as u32 };
+		let sp = st + 4*stacksize;
+		TCB_rw { state: TaskState::Ready, stack_top: st, stack_addr: sp }
+	}
+}*/
+
+
+/// realy like usize but usable in const eval
+type ptrsize = u32;
+const USIZEBYTES : ptrsize = 4; //(usize::BITS/8) as ptrsize;
+
+//         unsafe { &STACK[0] as *const u8 as u32 + $n }
+
+macro_rules! new_TCB_rw {
+    ($nwords:expr) => {{
+		#[link_section = ".bss"]  // .uninit
+        static mut STACK: [ptrsize; $nwords] = [0; $nwords];
+        let st  = unsafe { &STACK[0] as *const ptrsize as ptrsize };
+        let sp  = st + USIZEBYTES*$nwords;
+        TCB_rw { state: TaskState::Ready, stack_top: st, stack_addr: sp }
+	}};
+}
+//macro_rules! count {
+//    () => (0usize);
+//    ( $x:tt $($xs:tt)* ) => (1usize + count!($($xs)*));
+//}
 
 #[macro_export]
 macro_rules! Minos_Tasks {
-    ( $( < $name:ident, $prio:expr, $stacksize:tt > ),*) => {
-       static TOTO :u32 = 0;
+    ( $num:expr, $( < $name:ident, $prio:expr, $stacksize:tt > ),*) => {{
+ 		const DEFS : [TCB_ro; $num] = [  $( TCB_ro{stack_size:$stacksize, base_priority:$prio} ),* ];
+		static mut vars : [TCB_rw; $num] = [ $( new_TCB_rw!($stacksize) ),* ];
+		let pvars  = unsafe { &mut vars};
+		Minos_Scheduler { tasks_defs: &DEFS, tasks_vars: pvars }}
     };
 }
 
-Minos_Tasks!(<t1, 3, 1024>
-          ,  <t2, 4, 1024>);
+pub fn testme () -> Minos_Scheduler {
+	let s = Minos_Tasks!(2, <t1, 3, 1024> ,  <t2, 4, 1024>);
+    s
+}
+
 
 #[no_mangle]
 pub   extern "C" fn PendSV_Handler() {
@@ -123,7 +173,7 @@ pub fn minos_send_notif(_t:u8, _l :u32) {
 pub fn minos_wait() {
 }
 
-pub fn minos_wakeup(tasknum:u8) {
+pub fn minos_wakeup(_tasknum:u8) {
 }
 
 
